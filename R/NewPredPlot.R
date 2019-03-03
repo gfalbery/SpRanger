211412f9c8d904981e9bba29f3259bd7eb3539bb
Greg Albery <gfalbery@gmail.com>
2019-03-03 00:57:18 -0500

    Ohhhhh it's workin

--- R/NewPredPlot.R
+++ R/NewPredPlot.R
@@ -5,7 +5,8 @@ PredPlot <- function(Virus = NULL,
                      Threshold = 10,
                      Validate = TRUE,
                      Focal = c(1,0),
-                     Facet = FALSE){
+                     Facet = FALSE,
+                     Legend = "right"){
 
   require(dplyr); require(stringr); library(tidyverse); require(ggtree)
 
@@ -27,18 +28,20 @@ PredPlot <- function(Virus = NULL,
 
     Hosts <- Hosts %>% str_replace_all(" ", "_")
 
-    if(length(Hosts)<2) warning("This is only one host! Predictions might be rubbish.")
+    if(length(Hosts)<2) print("This is only one host! Predictions might be rubbish.", immediate = T)
 
-    if(length(intersect(Hosts, rownames(AllSims[[1]])))==0){
+    if(length(intersect(Hosts, rownames(AllSums)))==0){
 
       stop("None of your hosts are in our dataset! :( are they marine, or are there synonyms?")
 
-    } else if(length(intersect(Hosts, rownames(AllSims[[1]])))<length(Hosts)){
+    } else if(length(intersect(Hosts, rownames(AllSums)))<length(Hosts)){
 
-      warning(paste("Some hosts not found:",
-                    paste(setdiff(Hosts, rownames(AllSims[[1]]), collapse = ", ")),
-                    "; tread carefully"))
+      print(paste("Warning: Some hosts not found:",
+                  paste(setdiff(Hosts, rownames(AllSums)), collapse = ", "),
+                  "; tread carefully"))
     }
+
+    pHosts2 <- intersect(Hosts, rownames(AllSums))
 
     if(length(pHosts2)>0){
 
@@ -72,33 +75,38 @@ PredPlot <- function(Virus = NULL,
         GAMValid <- ValidEst %>%
           bind_rows() %>%
           group_by(Sp, Focal) %>% dplyr::summarise(Count = mean(Count)) %>%
-          slice(order(Count, decreasing = T))
+          slice(order(Count, decreasing = T)) %>%
+          mutate(Focal = as.factor(Focal))
 
       } else {
 
-        pHosts3 <- setdiff(colnames(FocalNet), pHosts2)
+        #pHosts3 <- setdiff(colnames(FocalNet), pHosts2)
+        #Estimates <- FocalNet[pHosts2, pHosts3]/length(AllSims)
 
-        Estimates <- FocalNet[pHosts2, pHosts3]/length(AllSims)
+        Estimates <- FocalNet[pHosts2,]/length(AllSims)
 
         if(is.null(dim(Estimates))) Estimates <- rbind(Estimates, Estimates)
 
         Ests <- data.frame(Sp = names(sort(colSums(Estimates), decreasing = T)),
-                           Count = sort(colSums(Estimates), decreasing = T)/nrow(Estimates))
+                           Count = sort(colSums(Estimates), decreasing = T)/nrow(Estimates)) %>%
+          mutate(Focal = ifelse(Sp%in%Hosts,1,0))
 
         rownames(Ests) <- Ests$Sp
 
         GAMValid <- Ests %>%
-          slice(order(Count, decreasing = T))
+          slice(order(Focal)) %>%
+          slice(order(Count, decreasing = T)) %>%
+          mutate(Focal = as.factor(Focal))
 
       }
 
     } else print("Hosts Not Found!")
 
-    Df <- GAMValid
+    Df <- GAMValid %>% as.data.frame()
     Df$Rank <- nrow(Df) - rank(Df$Count)
-    Df <- Df %>% mutate(Include = ifelse(Rank<Threshold,1,0))
+    Df <- Df %>% mutate(Include = ifelse(Rank<Threshold&Focal==0,1,0))
     if(1 %in% Focal) Df[Df$Focal==1,"Include"] <- 1
-    PredHosts <- Df %>% filter(Rank < Threshold) #%>% select(Sp) %>% unlist
+    PredHosts <- Df %>% filter(Include == 1) #%>% select(Sp) %>% unlist
 
   }
 
@@ -108,20 +116,19 @@ PredPlot <- function(Virus = NULL,
 
   VirusName <- str_replace_all(Virus, "_", " ")
 
-  Groups <- ifelse(STFull$tip.label%in%Hosts,"Known",ifelse(STFull$tip.label%in%PredHosts,"Predicted",""))
+  Groups <- ifelse(STFull$tip.label%in%Hosts,"Known",ifelse(STFull$tip.label%in%PredHosts$Sp,"Predicted",""))
 
   groupInfo <- split(STFull$tip.label, Groups)
   chiroptera <- groupOTU(STFull, groupInfo)
 
   plot2 <- ggtree(chiroptera, aes(color = group, alpha = group)) +
     scale_colour_manual(values = c("black", "red", "blue")) +
-    scale_alpha_manual(values = c(0.01,0.5,1)) +
-    theme(legend.position = "top")
+    scale_alpha_manual(values = c(0.01,0.5,1))
 
   g2 <- ggplotGrob(plot2)
 
-  xmin <- -1.9*(10^7)
-  xmax <- -1.2*(10^7)
+  xmin <- -2.1*(10^7)
+  xmax <- -1.45*(10^7)
   ymin <- -9*(10^6)
   ymax <- 9*(10^6)
 
@@ -137,8 +144,9 @@ PredPlot <- function(Virus = NULL,
       labs(
         title = paste("Predicted", VirusName, "Hosts")) +
       coord_fixed() +
-      scale_x_continuous(breaks = -10:10*2000000) +
-      scale_y_continuous(breaks = -5:5*2000000) +
+      theme(legend.position = Legend) +
+      scale_x_continuous(breaks = -10:10*5000000) +
+      scale_y_continuous(breaks = -5:5*5000000) +
       geom_polygon(inherit.aes = F, data = rect, aes(long, lat),
                    fill = "grey", colour = "grey") +
       annotation_custom(grob = g2,
@@ -151,11 +159,11 @@ PredPlot <- function(Virus = NULL,
       geom_polygon(data = WorldMap, inherit.aes = F, aes(long, lat, group = group), fill = "white", colour = "black") +
       geom_polygon(fill = NA, aes(colour = Host)) + # alpha = max(Rank)-Rank)) +
       labs(#alpha = "Inverse Rank",
-        title = paste("Predicted", VirusName, "Hosts")) +
+        title = paste0("Predicted", VirusName, " Hosts")) +
       coord_fixed() +
-      #theme(legend.position = "none") +
-      scale_x_continuous(breaks = -10:10*2000000) +
-      scale_y_continuous(breaks = -5:5*2000000) +
+      theme(legend.position = Legend) +
+      scale_x_continuous(breaks = -10:10*5000000) +
+      scale_y_continuous(breaks = -5:5*5000000) +
       geom_polygon(inherit.aes = F, data = rect, aes(long, lat),
                    fill = "grey", colour = "grey") +
       annotation_custom(grob = g2,
@@ -166,7 +174,16 @@ PredPlot <- function(Virus = NULL,
 
   }
 
-  if(Validate) return(list(MapPlot, ggplot(GAMValid, aes(Focal, MeanRank, colour = Focal)) + ggforce::geom_sina())) else{
+  if(Validate){
+
+    ValidPlot <- ggplot(Df, aes(Focal, Count, colour = Focal)) +
+      ggforce::geom_sina() +
+      geom_text(data = Df[1,], inherit.aes = F, aes(x = 1.5, y = max(Df$Count)*1.1, label = paste("Mean Focal Rank =", mean(Df[Df$Focal==1,"Rank"])))) +
+      ggtitle("Model Success Rate")
+
+    return(list(MapPlot,
+                ValidPlot))
+  }else{
 
     return(MapPlot)
 
