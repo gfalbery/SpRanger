4247ba846268a576e44e41f1383b97e6d10cbd01
Greg Albery <gfalbery@gmail.com>
2019-03-04 14:40:04 -0500

    Hopefully this will work

--- R/PairsWisely.R
+++ R/PairsWisely.R
@@ -1,72 +1,57 @@
 
-# Greg's range extraction function ####
+# Range overlap calculation ####
 
 PairsWisely <- function(Rasterstack, Species = "All"){
 
-  library(raster); library(tidyverse); library(Matrix)
-
+  library(raster)
+  library(tidyverse)
+  library(Matrix)
   t1 <- Sys.time()
-
   print("Getting the grid values")
-
   Valuedf <- data.frame(getValues(Rasterstack)) %>% as.matrix
   Valuedf[is.na(Valuedf)] <- 0
-
   Valuedf <- Valuedf %>% as("dgCMatrix")
 
-  if(Species != "All") Valuedf <- Valuedf[,Species]
+  print(paste0("Data frame size = ", dim(Valuedf)))
 
-  if(any(colSums(Valuedf)==0)){ print("Removing some species with no ranging data :(")
+  if (Species != "All")
+    Valuedf <- Valuedf[, Species]
 
-    Valuedf <- Valuedf[,-which(colSums(Valuedf)==0)]
+  print(paste0("Data frame size = ", dim(Valuedf)))
 
+  if (any(colSums(Valuedf) == 0)) {
+    print("Removing some species with no ranging data :(")
+    Valuedf <- Valuedf[, -which(colSums(Valuedf) == 0)]
   }
 
   RangeOverlap <- matrix(NA, nrow = ncol(Valuedf), ncol = ncol(Valuedf))
-  dimnames(RangeOverlap) <- list(colnames(Valuedf),colnames(Valuedf))
-
+  dimnames(RangeOverlap) <- list(colnames(Valuedf), colnames(Valuedf))
   print("Calculating Overlap")
-
-  for(x in 1:(ncol(Valuedf)-1)){
-
+  for (x in 1:(ncol(Valuedf) - 1)) {
     print(colnames(Valuedf)[x])
-
-    TrainGrids <- Valuedf[,x]
-
-    SubRangedf <- Valuedf[which(TrainGrids==1),x:ncol(Valuedf)]
-
-    if(!is.null(dim(SubRangedf))){
-
-      RangeOverlap[x,x:ncol(Valuedf)] <- apply(SubRangedf, 2, function(a) length(which(a==1)))
-
-    } else   RangeOverlap[x,x:ncol(Valuedf)] <- SubRangedf
-
+    TrainGrids <- Valuedf[, x]
+    SubRangedf <- Valuedf[which(TrainGrids == 1), x:ncol(Valuedf)]
+    if (!is.null(dim(SubRangedf))) {
+      RangeOverlap[x, x:ncol(Valuedf)] <- apply(SubRangedf,
+                                                2, function(a) length(which(a == 1)))
+    }
+    else RangeOverlap[x, x:ncol(Valuedf)] <- SubRangedf
   }
-
   x = ncol(Valuedf)
-
   print(colnames(Valuedf)[x])
-
-  TrainGrids <- Valuedf[,x]
-
-  SubRangedf <- Valuedf[which(TrainGrids==1),x:ncol(Valuedf)]
-
-  RangeOverlap[x,x:ncol(Valuedf)] <- length(which(SubRangedf==1))
-
-  FullRangeA = matrix(rep(diag(RangeOverlap), nrow(RangeOverlap)), nrow(RangeOverlap))
-  FullRangeB = matrix(rep(diag(RangeOverlap), each = nrow(RangeOverlap)), nrow(RangeOverlap))
-
-  RangeAdj <- RangeOverlap/(FullRangeA + FullRangeB - RangeOverlap) # Weighted evenly
-
+  TrainGrids <- Valuedf[, x]
+  SubRangedf <- Valuedf[which(TrainGrids == 1), x:ncol(Valuedf)]
+  RangeOverlap[x, x:ncol(Valuedf)] <- length(which(SubRangedf ==
+                                                     1))
+  FullRangeA = matrix(rep(diag(RangeOverlap), nrow(RangeOverlap)),
+                      nrow(RangeOverlap))
+  FullRangeB = matrix(rep(diag(RangeOverlap), each = nrow(RangeOverlap)),
+                      nrow(RangeOverlap))
+  RangeAdj <- RangeOverlap/(FullRangeA + FullRangeB - RangeOverlap)
   TimeTaken = Sys.time() - t1
-
   print(TimeTaken)
-
   diag(RangeAdj) <- NA
-
   RangeAdj[lower.tri(RangeAdj)] <- t(RangeAdj)[!is.na(t(RangeAdj))]
   diag(RangeAdj) <- 1
-
   return(RangeAdj)
-
 }
