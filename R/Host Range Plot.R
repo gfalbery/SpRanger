7d6741663656ec217e31a235db6d2187ab212aa7
Greg Albery <gfalbery@gmail.com>
2019-03-12 16:59:54 -0400

    Subdividing functions with a russian doll type thing

--- R/Host Range Plot.R
+++ R/Host Range Plot.R
@@ -1,36 +1,20 @@
 # Host Range Plot ####
 
-RangePlot <- function(Virus = NULL,
-                     HostList = NULL,
+RangePlot <- function(Mammals = NULL,
                      Map = TRUE,
-                     Tree = TRUE,
-                     Legend = "right"){
+                     Tree = TRUE){
 
   require(dplyr); require(stringr); library(tidyverse); require(ggtree); require(cowplot)
 
-  if(is.null(Virus)&is.null(HostList)) stop("One of Virus or HostList must be specified!")
-  if(!is.null(Virus)&!is.null(HostList)) stop("Only specify one of Virus or Host please :)")
+  if(any(!Mammals%in%FullPolygons$Host)) print(paste0("Some mammals not in our data:", paste(setdiff(Mammals, FullPolygons$Host), colllapse = ", ")))
 
-  if(!is.null(Virus)){
-
-    Virus <- Virus %>% str_replace_all(" ", "_")
-
-    if(!Virus%in%names(VirusAssocs)) stop("Virus not found :( try entering hosts manually?") else print("We have this virus! Wahey.")
-
-    HostList <- VirusAssocs[[Virus]]
-
-  }
-
-  if(any(!HostList%in%FullPolygons$Host)) print(paste0("Some hosts not in our data:", paste(setdiff(HostList, FullPolygons$Host), colllapse = ", ")))
-
-  RangePolygons <- FullPolygons %>% filter(Host%in%HostList)
+  RangePolygons <- FullPolygons %>% filter(Host%in%Mammals)
 
   VirusName <- str_replace_all(Virus, "_", " ")
 
   MapPlot <- ggplot(RangePolygons, aes(long, lat, group = paste(Host, group))) +
     geom_polygon(fill = NA, aes(colour = Host)) +
     coord_fixed() +
-    theme(legend.position = Legend) +
     scale_x_continuous(breaks = -10:10*5000000) +
     scale_y_continuous(breaks = -5:5*5000000)+
     theme(plot.title = element_text(hjust=0.5))
@@ -52,7 +36,7 @@ RangePlot <- function(Virus = NULL,
 
   if(Tree){
 
-    Groups <- ifelse(STFull$tip.label%in%HostList,"Known","")
+    Groups <- ifelse(STFull$tip.label%in%Mammals,"Known","")
 
     groupInfo <- split(STFull$tip.label, Groups)
     chiroptera <- groupOTU(STFull, groupInfo)
--- R/NewPredPlot.R
+++ R/NewPredPlot.R
@@ -4,7 +4,7 @@ PredPlot <- function(Virus = NULL,
                      HostList = NULL,
                      Threshold = 10,
                      Validate = TRUE,
-                     Focal = c(1,0),
+                     FocalDisplay = c(1,0),
                      Facet = FALSE,
                      Map = TRUE,
                      Tree = TRUE,
@@ -104,79 +104,34 @@ PredPlot <- function(Virus = NULL,
 
     Df <- ValidDF %>% as.data.frame()
     Df$Rank <- nrow(Df) - rank(Df$Count)
-    if(0 %in% Focal) Df <- Df %>% mutate(Include = ifelse(Rank<Threshold&Focal==0,1,0))
-    if(1 %in% Focal) Df[Df$Focal==1,"Include"] <- 1
+    if(1 %in% FocalDisplay) Df[Df$Focal==1,"Include"] <- 1
+    if(0 %in% FocalDisplay) Df <- Df %>% mutate(Include = ifelse(Rank<Threshold&Focal==0,1,0))
     PredHosts <- Df %>% filter(Include == 1) #%>% select(Sp) %>% unlist
 
   }
 
   Df <- Df %>% mutate(Focal = factor(c("Predicted","Observed")[(3-as.numeric(Focal))]))
 
-  PredHostPolygons <- FullPolygons %>% filter(Host%in%PredHosts$Sp) %>%
-    left_join(PredHosts, by = c("Host" = "Sp")) %>%
-    mutate(Host = factor(Host, levels = Df[order(Df$Rank, decreasing = TRUE),"Sp"] %>% unlist))
-
   VirusName <- str_replace_all(Virus, "_", " ")
-
-  MapPlot <- ggplot(PredHostPolygons, aes(long, lat, group = paste(Host, group))) +
-    geom_polygon(fill = NA, aes(colour = Host)) +
-    labs(
-      title = paste("Predicted", VirusName, "Hosts")) +
-    coord_fixed() +
-    theme(legend.position = Legend) +
-    scale_x_continuous(breaks = -10:10*5000000) +
-    scale_y_continuous(breaks = -5:5*5000000)+
-    theme(plot.title = element_text(hjust=0.5))
-
-  if(Map){
-
-    MapPlot <- ggplot(PredHostPolygons, aes(long, lat, group = paste(Host, group))) +
-      geom_polygon(data = WorldMap, inherit.aes = F, aes(long, lat, group = group), fill = "white", colour = "black") +
-      geom_polygon(fill = NA, aes(colour = Host)) +
-      labs(
-        title = paste("Predicted", VirusName, "Hosts")) +
-      coord_fixed() +
-      theme(legend.position = Legend) +
-      scale_x_continuous(breaks = -10:10*5000000) +
-      scale_y_continuous(breaks = -5:5*5000000)
-  }
-
-  if(Tree){
-
-    Groups <- ifelse(STFull$tip.label%in%HostList,"Known",ifelse(STFull$tip.label%in%PredHosts$Sp,"Predicted",""))
-
-    groupInfo <- split(STFull$tip.label, Groups)
-    chiroptera <- groupOTU(STFull, groupInfo)
-
-    plot2 <- ggtree(chiroptera, aes(color = group, alpha = group)) +
-      scale_colour_manual(values = c("black", "red", "blue")) +
-      scale_alpha_manual(values = c(0.01,0.5,1))
-
-    g2 <- ggplotGrob(plot2)
-
-    xmin <- -2.1*(10^7)
-    xmax <- -1.45*(10^7)
-    ymin <- -9*(10^6)
-    ymax <- 9*(10^6)
-
-    rectborder <- 60000
-    rect <- data.frame(long = c(xmin-rectborder, xmin-rectborder, xmax+rectborder, xmax+rectborder),
-                       lat = c(ymin-rectborder, ymax+rectborder, ymax+rectborder, ymin-rectborder))
-
-    MapPlot <- MapPlot +
-      geom_polygon(inherit.aes = F, data = rect, aes(long, lat),
-                   fill = "grey", colour = "grey") +
-      annotation_custom(grob = g2,
-                        xmin = xmin, xmax = xmax,
-                        ymin = ymin, ymax = ymax)
-  }
 
   if(Facet){
 
-    MapPlot <- MapPlot +
+    Plots <- list()
 
-      facet_wrap(~Focal, ncol = 1,
-                 labeller = labeller(Focal = c("0" = "Predicted", "1" ="Known")))
+    for(x in FocalDisplay){
+
+      Plots[[x]] <- Df %>% filter(Focal = c("Observed","Predicted")[FocalDisplay[x+1]]) %>%
+        select(Sp) %>% RangePlot(Map = Map, Tree = Tree) +
+        ggtitle(c("Observed","Predicted")[FocalDisplay[x+1]])
+
+    }
+
+    MapPlot <- Plots %>% arrange_ggplot2(nrow = 2)
+
+  } else {
+
+    MapPlot <- Df %>% select(Sp) %>% RangePlot(Map = Map,
+                                               Tree = Tree)
 
   }
 
