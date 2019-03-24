7d61e907c80de4c3d915b3c16246074e5b0e1311
Greg Albery <gfalbery@gmail.com>
2019-03-24 19:26:43 +0000

    Adding proper predicate option

--- R/Range Intersections.R
+++ R/Range Intersections.R
@@ -5,25 +5,7 @@ IntersectGet <- function(Rasterstack, Names = "All", Predicate = F){
 
   N = length(Names)
 
-  if(Predicate){
-
-    lapply(1:N, function(a){
-
-      print(Names[a])
-
-      lapply(1:N, function(b){
-
-        if(a<b){
-
-          if(RangeAdj[Names[a],Names[b]]>0){
-
-            raster::intersect(MammalRanges[[Names[a]]], MammalRanges[[Names[b]]])
-
-          }
-        }
-      })
-    })
-  } else {
+  if(!Predicate){
 
     lapply(1:N, function(a){
 
@@ -35,6 +17,30 @@ IntersectGet <- function(Rasterstack, Names = "All", Predicate = F){
 
           raster::intersect(MammalRanges[[Names[a]]], MammalRanges[[Names[b]]])
 
+        }
+
+      })
+    })
+  } else {
+
+    if(length(setdiff(Names, rownames(Predicate))>0)){
+      Names <- intersect(Names, colnames(Predicate))
+      print("Warning: Some species not in predicate matrix!!")
+    }
+
+    lapply(1:N, function(a){
+
+      print(Names[a])
+
+      lapply(1:N, function(b){
+
+        if(a<b){
+
+          if(Predicate[Names[a],Names[b]]>0){
+
+            raster::intersect(MammalRanges[[Names[a]]], MammalRanges[[Names[b]]])
+
+          }
         }
       })
     })
